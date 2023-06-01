323429155c967ef642d77a2b7a5d0f0c9ae2af71
Oleksandra Serkes <alexsandra.serkes@gmail.com>
2023-06-01 23:20:57 +0300

    added functionality to ListTodo component

--- src/components/ListTodo/ListTodo.js
+++ src/components/ListTodo/ListTodo.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import CreateTodo from '../CreateTodo/CreateTodo';
 
 export default function ListTodo({ todo, setTodo }) {
-  const [isChecked, setIsChecked] = useState(todo.checked);
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState('');
 
   const deleteTodo = (id) => {
     const newTodo = [...todo].filter((item) => item.id !== id);
@@ -12,39 +12,80 @@ export default function ListTodo({ todo, setTodo }) {
   const statusTodo = (id) => {
     const newTodo = [...todo].filter((item) => {
       if (item.id === id) {
-        item.status = !item.status;
+        item.checked = !item.checked;
       }
       return item;
     });
     return setTodo(newTodo);
   };
 
-  return (
-    <div>
-      {todo.length > 0 ? (
-        <ul className='todo_list'>
-          {todo.map((item) => (
-            <li key={item.id}>
-              <div className='todo'>
-                <input
-                  className='status_btn'
-                  type='checkbox'
-                  name='check'
-                  id='check'
-                  defaultChecked={todo.checked}
-                  onClick={() => statusTodo()}
-                />
-              </div>
-              <label htmlFor='check'>{item.title}</label>
+  const editTodo = (id, title) => {
+    setEdit(id);
+    setValue(title);
+  };
+
+  const saveTodo = (id) => {
+    let newTodo = [...todo].map((item) => {
+      if (item.id === id) {
+        item.title = value;
+      }
+      return item;
+    });
+    setTodo(newTodo);
+    setEdit(null);
+  };
+
+  const renderList = todo.map((item) => (
+    <li key={item.id}>
+      <div className='todo'>
+        {edit === item.id ? (
+          <div>
+            <input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            ></input>
+          </div>
+        ) : (
+          <label htmlFor='check'>{item.title}</label>
+        )}
+        {edit === item.id ? (
+          <button onClick={() => saveTodo(item.id)}>Save</button>
+        ) : (
+          <>
+            <div>
+              <input
+                className='status_btn'
+                type='checkbox'
+                name='check'
+                id='check'
+                defaultChecked={item.checked}
+                onClick={() => statusTodo(item.id)}
+              />
+            </div>
+            <div>
+              <button
+                className='edit_btn'
+                onClick={() => editTodo(item.id, item.title)}
+              >
+                Edit todo
+              </button>
               <button
                 className='delete_btn'
                 onClick={() => deleteTodo(item.id)}
               >
-                Delete
+                Delete todo
               </button>
-            </li>
-          ))}
-        </ul>
+            </div>
+          </>
+        )}
+      </div>
+    </li>
+  ));
+
+  return (
+    <div>
+      {todo.length > 0 ? (
+        <ul className='todo_list'>{renderList}</ul>
       ) : (
         <div className='empty'>
           <p>There is no task</p>
